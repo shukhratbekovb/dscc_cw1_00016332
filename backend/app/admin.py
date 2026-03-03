d62da61ce2758a4237466a4aba8e0340ed9f853f
boburbek <boburbek@MacBook-Air-boburbek.local>
2026-03-03 13:39:48 +0500

    added Admin Panel

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from app.models import Tag, Project, Todo
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ('name',)
+    ordering = ('name',)
+    list_filter = ('user',)
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'project', 'status', 'created_at')
+    search_fields = ('name',)
+    list_filter = ('user', 'project', 'status')
+    ordering = ('name', 'created_at', 'status')
 
