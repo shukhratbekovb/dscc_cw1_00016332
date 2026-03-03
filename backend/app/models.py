fd9f41998d0345fc8748065a81809a411233abaf
boburbek <boburbek@MacBook-Air-boburbek.local>
2026-03-03 13:34:58 +0500

    Added Models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Tags"
+
+
+class Todo(models.Model):
+    STATUS = (
+        (
+            "process", "Process"
+        ),
+        (
+            "completed", "Completed"
+        ),
+        (
+            "new", "New"
+        ),
+        (
+            "canceled", "Canceled"
+        )
+    )
+
+    title = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    status = models.CharField(max_length=20, choices=STATUS, default="new")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Todos"
